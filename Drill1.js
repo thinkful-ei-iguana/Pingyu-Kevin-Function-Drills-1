f43dbaf4a6bee7a81fc84226e9b2a89f0750980e
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-21 15:09:01 -0400

    Takes arguments

--- Drill1.js
+++ Drill1.js
@@ -1,5 +1,5 @@
-function createGreeting() {
-    return 'Hello';
+function createGreeting(name, age) {
+    return 'Hello, my name is ' + name + 'and I am ' + age + '.';
 }
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Kevin', 30);
 console.log(greeting1);
