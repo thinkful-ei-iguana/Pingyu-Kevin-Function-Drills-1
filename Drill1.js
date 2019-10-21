aef1b1bb859efba364fd0f2de9af98a15522cd02
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-21 14:58:10 -0400

    Console log message

---
+++ Drill1.js
@@ -0,0 +1,5 @@
+//function indtroduction(name = 'Kevin', age = 30) {
+//console.log(`Hi, my name is ${name} and I am ${age} years old.`);
+//}
+
+console.log('Hi, my name is Kevin and I am 30 years old.');
