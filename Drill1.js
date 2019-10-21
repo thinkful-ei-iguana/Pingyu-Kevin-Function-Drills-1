639bea30b52ed90b28b9306d8c7b71b91b2ba52c
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-21 15:06:42 -0400

    Added function

--- Drill1.js
+++ Drill1.js
@@ -1,5 +1,5 @@
-//function indtroduction(name = 'Kevin', age = 30) {
-//console.log(`Hi, my name is ${name} and I am ${age} years old.`);
-//}
-
-console.log('Hi, my name is Kevin and I am 30 years old.');
+function createGreeting() {
+    return 'Hello';
+}
+const greeting1 = createGreeting();
+console.log(greeting1);
